960bb00bd490fd018d215dca5b54dbe56b22cba8
wheatup <acphun@gmail.com>
2019-02-08 11:56:12 +0900

    what?

--- pages/array.php
+++ pages/array.php
@@ -3,20 +3,22 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Syntax</title>
+	<title>Array</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" media="screen" href="../css/main.css" />
 	<link href="https://fonts.googleapis.com/css?family=Varela" rel="stylesheet">
 </head>
 <body>
 	<?php
-		$greets = "Hello World";
-		echo("<h1 class=\"heading\">$greets</h1>");
-		echo(str_replace("World", "Hell", $greets));
-		echo('<br/>');
-		$arr = ['Element 1', 'Element2', 'Element 3'];
-		$arr[] = 'Element 4';
-		echo($arr[3]);
+		// array
+		$friends = ['Alice', 'Bob', 'Charlie', 'Daisy'];
+		echo("<span>$friends[0]</span>");
+		echo('<br>');
+
+		// associated array
+		$items = ['name' => 'Alice', 'age' => 20];
+		echo("<span>$items[age]</span>");
+		echo('<br>');
 	?>
 </body>
 </html>
--- pages/form.php
+++ pages/form.php
@@ -6,23 +6,35 @@
 	<title>Form</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" media="screen" href="../css/main.css" />
-	<link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css?family=Varela" rel="stylesheet">
 </head>
 <body>
-	<form class="form" action="./form.php" method="get">
-		<h2 class="form--title">Basic Information<h2>
-		<div class="form--group">
-			<label for="name">Name: </label>
-			<input type="text" id="name" name="name" placeholder="Alex" value=<?php echo ($_GET['name']) ?> />
-		</div>
-		<div class="form--group">
-			<label for="age">Age: </label>
-			<input type="number" id="age" name="age" placeholder="24" value=<?php echo ($_GET['age']) ?> />
-		</div>
-		<button class="form--button">submit</button>
-		<p>
-		<?php echo ($_GET['name'] . ' is ' . $_GET['age'] . ' years old.'); ?>
-		</p>
-	</form>
+	<section class="form">
+		<form class="form--form" action="./form.php" method="post">
+			<h2 class="form--form--title">Basic Information<h2>
+			<div class="form--form--group">
+				<label for="name">Name: </label>
+				<input type="text" id="name" name="name" placeholder="Please enter your name" value="<?php
+					if (array_key_exists('name', $_POST)) {
+						echo ($_POST['name']);
+					}
+				?>">
+			</div>
+			<div class="form--form--group">
+				<label for="age">Age: </label>
+				<input type="number" id="age" name="age" placeholder="Please enter your age" value="<?php
+					if (array_key_exists('age', $_POST)) {
+						echo ($_POST['age']);
+					}
+				?>">
+			</div>
+			<button class="form--form--button">submit</button>
+			<p><?php
+				if (array_key_exists('name', $_POST) && array_key_exists('age', $_POST)) {
+					echo ($_POST['name'] . ' is ' . $_POST['age'] . ' years old.');
+				}
+			?></p>
+		</form>
+	</section>
 </body>
 </html>
